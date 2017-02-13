7f3bfe82111e1ce17c2dd73e98ebb1943d1f1a7f
nick11roberts <n3robert@ucsd.edu>
2017-02-13 13:03:59 -0800

    Added button toggling functionality for attending event.

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,13 @@ $("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
+
+$('.attend-button').click(function() {
+  $(this).toggleClass("active");
+
+  $(this).css('width', $(this).outerWidth());
+
+  $(this).text(function(i, text){
+    return text === "Cancel" ? "I'm going" : "Cancel";
+  })
+ });
