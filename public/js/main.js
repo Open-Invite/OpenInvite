ac13d18e3e577b1ebe406d3f1ea3925f3b31cf81
nick11roberts <n3robert@ucsd.edu>
2017-02-17 11:17:21 -0800

    Fixed button width issue after closing nav drawer.

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ $("#menu-toggle").click(toggleNav);
 $('.attend-button').click(function() {
   $(this).toggleClass("active");
 
-  $(this).css('width', $(this).outerWidth());
+  //$(this).css('width', $(this).outerWidth());
 
   $(this).text(function(i, text){
     return text === "Cancel" ? "I'm going" : "Cancel";
